f713825a53b11570c0d7d6b1aa3655914042d794
OscStal <oscsta@student.chalmers.se>
2022-02-22 16:20:55 +0100

    fix

--- dataset_creation.py
+++ dataset_creation.py
@@ -2,7 +2,7 @@ import cv2
 import os
 
 
-def imgs_from_video(file_path: str) -> None:
+def imgs_from_video(file_path: str, file_prefix: str) -> None:
     video_capture = cv2.VideoCapture(file_path)
     fps = video_capture.get(cv2.CAP_PROP_FPS)
     rate: int = int(fps / image_capture_rate)
@@ -11,7 +11,7 @@ def imgs_from_video(file_path: str) -> None:
     count: int = 0
     while success:
         if (count % rate) == 0:
-            cv2.imwrite(f"image{count//rate}.jpg", image)
+            cv2.imwrite(f"{file_prefix}{count//rate}.jpg", image)
             print(f"Saved image {count//rate} from frame {count}")
         success, image = video_capture.read()
         print(f"Created frame: {success}")
@@ -20,15 +20,16 @@ def imgs_from_video(file_path: str) -> None:
 
 
 # Specify video path and output folder here
-vid_file_path = r"datasets\one\vids\4_flat.mp4"
-out_folder_path = r"datasets\one\imgs"
+vid_file_path = r"datasets\linemod\vids\datasetfilm3.mp4"
+out_folder_path = r"datasets\linemod\imgs"
 
 # Also specify this, approximate amount of images captured per second of video
-image_capture_rate: int = 5
+image_capture_rate: int = 2
+image_filename_prefix = "datasetfilm3_"
 
 
 
 if __name__ == "__main__":
     if not os.path.exists(out_folder_path):
         os.makedirs(out_folder_path)
-    imgs_from_video(vid_file_path)
+    imgs_from_video(vid_file_path, image_filename_prefix)
