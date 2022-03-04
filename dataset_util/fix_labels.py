c0cdff725e26ad20ce2d586a691287fc4ff9bf4b
OscStal <oscsta@student.chalmers.se>
2022-03-04 10:55:22 +0100

    fix

--- dataset_util/fix_labels.py
+++ dataset_util/fix_labels.py
@@ -3,17 +3,33 @@ import json
 
 
 
-directory = "datasets/xxx"
+directory = "datasets/sample/imgs/all"
 
 
 
 def fix_label(filename: str):
-    data = json.load(filename)
+    data = None
+
+    # Load json file
+    with open(directory + "/" + filename, "r") as json_file:
+        data = json.load(json_file)
+
+    # Overwrite volo with volvo
     for shape in data["shapes"]:
-        if "volo" in shape["label"]:
-            shape["label"].replace("volo", "volvo")
+        if shape["label"].startswith("volo"):
+            shape["label"] = "volvo_part1"
+
+    # Save to file        
+    with open(directory + "/" + filename, "w") as json_file:
+        json.dump(data, json_file, indent=4)
+
+
 
 if __name__ == "__main__":
-    for filename in os.scandir(directory):
-        if ".json" in filename:
+    fixed_files = 0
+    for filename in os.listdir(directory):
+        if filename.endswith(".json") and not filename.startswith(("all", "train", "val", "valid", "validate")):
             fix_label(filename)
+        fixed_files += 1
+    
+    print(f"Files scanned: {fixed_files}")
