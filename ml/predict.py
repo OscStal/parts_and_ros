285cf59498fac4eff103c39a8ce56603a8438c31
OscStal <oscsta@student.chalmers.se>
2022-04-04 13:17:33 +0200

    refactoring

--- ml/predict.py
+++ ml/predict.py
@@ -23,18 +23,17 @@ from scipy import ndimage
 
 
 
-def detect_viz():
+def detect_objects():
     """
     Sets up config and model, creates Predictor, runs predictions on an image captured by camera and visualizes it
     """
 
     # Setup metadata and model
-    test_data_location = "datasets/sample/imgs/all"
     register_coco_instances("train", {}, os.path.join(test_data_location, "a_json.json"), test_data_location)
     custom_metadata: Metadata = MetadataCatalog.get("train")
 
     # Config Setup, Inference should use the config with parameters that are used in training
-    # Some of these config parameters might be unneccesary for testing on images
+    # Some of the config parameters might be unneccesary for testing on images
     cfg: CfgNode = get_cfg()
     setup_cfg(cfg)
 
@@ -56,46 +55,30 @@ def detect_viz():
         # frame = cv2.imread("test1.jpeg")
 
         print("Calculating outputs")
-        if benchmark and visualize: t0 = time.perf_counter()
-        if visualize: 
-            v = Visualizer(frame[:, :, ::-1],
-                    metadata=custom_metadata, 
-                    scale=1.0, 
-                    instance_mode=ColorMode.SEGMENTATION   # remove the colors of unsegmented pixels. This option is only available for segmentation models
-            )
-
         if benchmark: t1 = time.perf_counter()
         outputs = predictor(frame) # format is documented at https://detectron2.readthedocs.io/tutorials/models.html#model-output-format
         if benchmark: t2 = time.perf_counter()
 
-        # Finds and plots a circle in the center of mass of the binary mask
-        find_center(outputs, frame)
-        # cv2.waitKey(0) & 0xFF == ord('n')
-
-        if device == "cuda":
-            shape = np.array(outputs['instances'].pred_masks.cpu()).shape
-        if device == "cpu":
-            np.array(outputs['instances'].pred_masks).shape
+        shape = get_shape(outputs)
         print(f"Detected objects + image size: {shape}")
 
-        # print("Creating drawable instance predictions")
-        if visualize:
-            preds = v.draw_instance_predictions(outputs["instances"].to("cpu"))
-            pred_img = preds.get_image()[:, :, ::-1]
+        visualize_all_objects(outputs, frame, custom_metadata)
 
-            print("Showing image")
-            cv2.imshow("", pred_img)
-            cv2.waitKey(10)
+        for mask in outputs['instances'].pred_masks:
+            print(np.array(mask.shape))
+            find_center(mask, frame)
+
+        # Finds and plots a circle in the center of mass of the binary mask
+        # cv2.waitKey(0) & 0xFF == ord('n')
+
 
         if benchmark: 
-            tt0 = t1 - t0
-            print(f"Time to create visualizer: {tt0}")
             tt1 = t2 - t1
             print(f"Time for prediction: {tt1}")
             time_list.append(tt1)
         iters += 1
 
-        if (cv2.waitKey(1000) & 0xFF == ord('q')) or iters > 20:
+        if (cv2.waitKey(1000) & 0xFF == ord('q')) or iters > 5:
             cap.release()
             cv2.destroyAllWindows()
             if benchmark:
@@ -103,10 +86,37 @@ def detect_viz():
                 print(f"Average time: {avg_time}")
             break
 
+
+
+def get_shape(outputs):
+    if device == "cuda":
+        shape = np.array(outputs['instances'].pred_masks.cpu()).shape
+    if device == "cpu":
+        shape = np.array(outputs['instances'].pred_masks).shape
+    return shape
+
+
+def visualize_all_objects(outputs, frame, custom_metadata):
+    if visualize:
+        v = Visualizer(frame[:, :, ::-1],
+                    metadata=custom_metadata, 
+                    scale=1.0, 
+                    instance_mode=ColorMode.SEGMENTATION   # remove the colors of unsegmented pixels. This option is only available for segmentation models
+        )
+
+        # print("Creating drawable instance predictions")
+        preds = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+        pred_img = preds.get_image()[:, :, ::-1]
+
+        print("Showing image")
+        cv2.imshow("", pred_img)
+        cv2.waitKey(10)
+
+
 def setup_cfg(cfg):
     cfg.merge_from_file(model_zoo.get_config_file("COCO-InstanceSegmentation/mask_rcnn_R_50_DC5_3x.yaml"))
     cfg.MODEL.WEIGHTS = model_path
-    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.5
+    cfg.MODEL.ROI_HEADS.SCORE_THRESH_TEST = 0.85
     cfg.MODEL.DEVICE = device
     cfg.DATASETS.TRAIN = ("train", )
     cfg.DATALOADER.NUM_WORKERS = 2
@@ -117,21 +127,21 @@ def setup_cfg(cfg):
     cfg.MODEL.ROI_HEADS.NUM_CLASSES = 1
 
 
-def find_center(outputs, image):
+def find_center(mask, image):
     """
     Finds the "center of mass" of the binary mask from a prediction
     """
     # numpy.set_printoptions(threshold=np.inf)  # Print entire binary mask
 
     if device == "cuda":
-        mask = np.array(outputs["instances"].pred_masks.cpu())[0]
+        mask = np.array(mask.cpu())
     if device == "cpu":
-        mask = np.array(outputs["instances"].pred_masks)[0]
-    print(f"Mask is {mask}")
+        mask = np.array(mask)
+    # print(f"Mask is {mask}")
 
     center = ndimage.measurements.center_of_mass(mask)
     # center has coordinates in (y,x) and float, this makes it (x,y) and rounded integers
-    center_int = tuple((int(x) for x in center)[::-1])
+    center_int = tuple((int(x) for x in center))[::-1]
     # center_int_yx = tuple(int(x) for x in center)
     # center_int_xy = center_int_yx[::-1]
     print(f"Center pixel: {center_int}")
@@ -146,9 +156,10 @@ def find_center(outputs, image):
 
 vidcap_id: int = 1
 model_path: str = "model_final.pth"
+test_data_location = "datasets/sample/imgs/all"
 device: str = "cpu"
-benchmark: bool = True
-visualize: bool = True
+benchmark: bool = False
+visualize: bool = False
 
 if __name__ == "__main__":
-    detect_viz()
+    detect_objects()
